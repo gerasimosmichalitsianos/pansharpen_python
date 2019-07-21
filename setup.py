1ea1fdf0f57bbcc40ffe920232ccca98b76ae830
Gerasimos Michalitsianos <gerasimosmichalitsianos@gmail.com>
2019-07-21 14:04:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-  name='pansharpen',
+  name='python-pansharpen',
   version='1.0.0',
   scripts=['bin/pansharpen.py',], 
   license='MIT',
   include_package_data=True, 
   long_description=open('README.txt').read(),
   entry_points = {
-    'console_scripts': ['pansharpen=pansharpen:main'],
+    'console_scripts': ['python-pansharpen=pansharpen:main'],
   }
 )
